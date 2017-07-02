f52f10a9868339b2dbce24f22ac5715e783d887d
wentjx <wentjx@googlemail.com>
2017-07-02 10:57:15 +0200

    bugfix

--- boot/98_dns_install.lua
+++ boot/98_dns_install.lua
@@ -1,5 +1,5 @@
-local rs=require("filesystem")
-filesystem.copy("/home/.shrc", "/home/.srhc.bck")
+local fs=require("filesystem")
+fs.copy("/home/.shrc", "/home/.srhc.bck")
 dat = io.open("/home/.shrc", "a")
 dat:write("/usr/bin/dnsinstall")
 dat:close()
